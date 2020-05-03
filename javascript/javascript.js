7f0d5d66c27a3733334786a30ce2efd1ecd5d960
Joshua <hosanajoshua@gmail.com>
2020-05-03 19:03:46 +0300

    added table data for user reference

---
+++ javascript/javascript.js
@@ -0,0 +1,9 @@
+var siblings = ["Morgan","Diana","Emmanuel","Eunice","Joshua"]
+var parents = ["Maclaud Ago Ombima","Jones Wanda Joshua"]
+siblings.concat(parents)
+var pets = []
+pets.push("cat")
+pets.push("dog")
+siblings.reverse()
+parents.reverse()
+parents[0];
