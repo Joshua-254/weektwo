28683fe44c99a31fa447a0d0244157ed27562609
Joshua <hosanajoshua@gmail.com>
2020-05-04 19:07:34 +0300

    Replaced the javascript folder that had unneeded file

--- javascript/javascript.js
+++
@@ -1,9 +0,0 @@
-var siblings = ["Morgan","Diana","Emmanuel","Eunice","Joshua"]
-var parents = ["Maclaud Ago Ombima","Jones Wanda Joshua"]
-siblings.concat(parents)
-var pets = []
-pets.push("cat")
-pets.push("dog")
-siblings.reverse()
-parents.reverse()
-parents[0];
