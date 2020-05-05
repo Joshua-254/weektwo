4122e989c0e083c63bdb70a6164171e2fd20619e
Joshua <hosanajoshua@gmail.com>
2020-05-05 11:32:23 +0300

    Made icon to the tittle

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,22 +1,18 @@
 
+
+function akanName(){
 //uer input data collection center
-function purple =(){
-var x = document.getElementById("userinput").elements.namedItem("year").value;
-var x = document.getElementById("userinput").elements.namedItem("century").value;
-var x = document.getElementById("userinput").elements.namedItem("month").value;
-var x = document.getElementById("userinput").elements.namedItem("day").value;
-}
+var Y = document.getElementById("userinput").elements.namedItem("year").value;
+var C = document.getElementById("userinput").elements.namedItem("century").value;
+var M = document.getElementById("userinput").elements.namedItem("month").value;
+var D = document.getElementById("userinput").elements.namedItem("day").value;
 //variable names definition
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","kwame"]
 var femaleNames = ["Akosua","Adwoa","Abenna","Akua","Yaa","Afua","Ama"]
 var weekDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 var months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 
-var C = century
-var Y = Year
-var M = Month
-var D = Day of the month
-
+}
 //calculator
 //Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
 //where;
