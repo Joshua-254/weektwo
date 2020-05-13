b0f4bb67162562f24783bc65ea5817010d43042b
Joshua <hosanajoshua@gmail.com>
2020-05-13 19:06:25 +0300

    Added the male gender selection section and the various allert messages

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -42,3 +42,29 @@ var yy = year.slice(0, 2);
 var yy = year.slice(2, 4);
 var cc = (yy - 1) / 100 + 1;
 var dayOfTheWeek = parseInt(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
+
+//male section selection
+if (male) {
+if (dayOfTheWeek == 1) {
+  document.getElementById("results").innerHTML =
+    alert("Hello, You were born on Monday and your Akan name is.. " + boy[1]);
+} else if (dayOfTheWeek == 2) {
+  document.getElementById("results").innerHTML =
+    alert("Hello, You were born on Tuesday and your Akan name is.. " + boy[2]);
+} else if (dayOfTheWeek == 3) {
+  document.getElementById("results").innerHTML =
+    alert("Hello, You were born on Wednesday and your Akan name is.. " + boy[3]);
+} else if (dayOfTheWeek == 4) {
+  document.getElementById("results").innerHTML =
+    alert("Hello, You were born on Thursday and your Akan name is.. " + boy[4]);
+} else if (dayOfTheWeek == 5) {
+  document.getElementById("results").innerHTML =
+    alert("Hello, You were born on Friday and your Akan name is.. " + boy[5]);
+} else if (dayOfTheWeek == 6) {
+  document.getElementById("results").innerHTML =
+    alert("Hello, You were born on Saturday and your Akan name is.. " + boy[6]);
+} else if (dayOfTheWeek == 0) {
+  document.getElementById("results").innerHTML =
+    alert("Hello, You were born on Sunday and your Akan name is.. " + boy[0]);
+}
+}
