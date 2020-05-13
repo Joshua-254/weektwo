be722a5b2d2a8f60e67ea397e6a633638e20a7a0
Joshua <hosanajoshua@gmail.com>
2020-05-13 18:47:57 +0300

    Added a calculator logic to the form input collection

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -14,5 +14,12 @@
  ////OUTPUT REQUIRED
 //Akan Name 
 
+//variable name section
 var maleGender = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleGender = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+var dd = document.getElementById("day").value;
+var year = document.getElementById("year").value;
+var mm = document.getElementById("month").value;
+var male = document.getElementById("male").checked;
+var female = document.getElementById("female").checked;
